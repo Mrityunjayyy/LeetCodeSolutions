c4b2a79d5a9aa857633cc2d6f66073d1a08afed9
Mrityunjay <122476969+Mrityunjayyy@users.noreply.github.com>
2023-08-09 23:58:56 +0530

    Time: 2430 ms (5.08%), Space: 64.3 MB (25.6%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -0,0 +1,86 @@
+class Solution {
+    static int[][] t = new int[2001][2001];
+    static boolean[][] palindrome = new boolean[2001][2001];
+    public int minCut(String s) {
+        
+        for(int[] a : t)
+        {
+            Arrays.fill(a , -1);
+        }
+
+        return solve(s , 0, s.length() -1);
+    }
+
+    public int solve(String s , int i , int j)
+    {
+        if(t[i][j] != -1)
+        return t[i][j];
+
+        if(i >= j || isPalindrome(s , i , j) == true)
+        {
+            t[i][j] = 0;
+            return 0;
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for(int k = i ; k <= j-1 ; k++)
+        {
+            // int left;
+            // if(t[i][k] != -1)
+            // { 
+            //     left = t[i][k];
+            // }
+            // else 
+            // {
+            //     left = solve(s , i , k);
+            //     t[i][k] = left;
+            // }
+
+            // int right;
+            // if(t[k+1][j] != -1)
+            // {
+            //     right = t[k+1][j];
+            // }
+            // else 
+            // {
+            //     right = solve(s , k+1 , j);
+            //     t[k+1][j] = right;
+            // }
+
+            if(isPalindrome(s , i ,k) == true)
+            {   int temp_ans = 1 + solve(s , k+1 , j);
+
+            if(temp_ans  < min)
+            {
+                min = temp_ans;
+            }
+            }
+        }
+
+        return t[i][j] = min;
+    }
+
+
+     boolean isPalindrome(String s , int i  , int j)
+    {
+        if(palindrome[i][j] == true)  return true;
+
+        if(i < 0 || j >= s.length()) return false;
+        
+        if(i == j) return true;
+           
+        while(i < j)
+        {
+            if(s.charAt(i) != s.charAt(j))
+            {
+                return false;
+            }
+            
+            i++;
+            j--;
+        }
+        
+        return palindrome[i][j] = true;
+    }
+}
